cc31ca8959da1c0ca27a25316e09a4b6c886be8c
Ilan Kleinberger <ilankl@gmail.com>
2020-08-03 12:07:48 +0300

    static server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 var port = 3000;
-app.use('/', express.static(__dirname + './'));
+app.use('/', express.static(__dirname));
 
 app.set('port', (process.env.PORT || port));
 app.listen(app.get('port'),function(){
