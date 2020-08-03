839d20057eedf396a02b40cc5e3311e12a598b3a
Ilan Kleinberger <ilankl@gmail.com>
2020-08-03 12:05:41 +0300

    static server

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
 var express = require('express');
-
+var app = express();
+var port = 3000;
 app.use('/', express.static(__dirname + './'));
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || port));
 app.listen(app.get('port'),function(){
-    console.log('Uniper - UniManage - Front End running on port: ' + port);
+    console.log('server running on port: ' + port);
 });
