030424ae58644622455c822b407389c19b2baacc
Ilan Kleinberger <ilankl@gmail.com>
2020-08-03 12:10:13 +0300

    podt for SSL

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 var express = require('express');
 var app = express();
-var port = 3000;
+var port = 443;
 app.use('/', express.static(__dirname));
 
 app.set('port', (process.env.PORT || port));
